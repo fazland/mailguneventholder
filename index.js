6b3a74ba8cb25906a5a76f6b3ca21b301a6f778d
Giovanni Albero <giovanni.albero@fazland.com>
2016-05-30 16:34:28 +0200

    Introduced a new parameter to start retrieve process from specifc message-id

--- index.js
+++ index.js
@@ -8,27 +8,33 @@ var config = require('./config');
 program
     .option('-a, --apikey <api-key>', 'Mailgun\'s api-key')
     .option('-d, --domainname <domain-name>', 'Domain name to retrieve event log')
+    .option('-i, --messageId <message-id>', 'Start message id, from this id start to retrieve events')
     .parse(process.argv)
 ;
-
 
 var initialUrl = 'https://api.mailgun.net/v3/' + program.domainname + '/events';
 
 doRequest(initialUrl);
 
 function doRequest(url) {
+    console.log(url);
     request
         .get(
             {
                 url: url,
                 qs: {
-                    "limit": 300
+                    "limit": 300,
+                    "message-id": program.messageId
                 }
             }, function (error, response, body) {
                 jsonObj = JSON.parse(body);
-
-                mongodbHelper.db.collection(config.mongodb.collection).insertMany(jsonObj.items);
-                console.log('inserted %d', jsonObj.items.length);
+                console.log(jsonObj);
+                if (program.messageId == null) {
+                    mongodbHelper.db.collection(config.mongodb.collection).insertMany(jsonObj.items);
+                    console.log('inserted %d', jsonObj.items.length);
+                } else {
+                    program.messageId = null;
+                }
                 if (url !== jsonObj.paging.next) {
                     doRequest(jsonObj.paging.next);
                 }
